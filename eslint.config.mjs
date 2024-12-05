071b63b331bbacc1846bfff73d13f355dcba24b2
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2024-12-05 19:31:34 -0300

    Update ESLint configuration to extend recommended settings and adjust magic number rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,10 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-    eslint.configs.recommended,
-    tseslint.configs.recommended,
-);
+export default {
+    extends: [eslint.configs.recommended, tseslint.configs.recommended],
+    rules: {
+        "no-magic-numbers": "off",
+        "@typescript-eslint/no-magic-numbers": "error",
+    },
+};
